65471efc42d53ac1bd8e6787f8ba3794c54bbed3
Josiah Hunsinger <josiah.hunsinger@gmail.com>
2017-09-03 19:09:01 -0400

    got custom info from the server showing on web page with AJAX

--- server.py
+++ server.py
@@ -2,13 +2,20 @@ from flask import Flask, jsonify, render_template, request
 from tournament import Tournament
 from threading import Thread
 
+
+player_list = []
+with open('players.txt', 'r') as f:
+    for line in f:
+        player_list.append(line.strip())
+t = Tournament(player_list)
+play_status = {'table1': 'game1', 'table2':' game2', 'next_up': 'game3'}
+
+
 app = Flask(__name__)
 
-@app.route('/_add_numbers')
+@app.route('/_get_status')
 def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+    return jsonify(play_status)
 
 @app.route('/')
 def index():
@@ -18,12 +25,6 @@ server_thread = Thread(target=app.run)
 server_thread.start()
 
 if __name__ == '__main__':
-    player_list = []
-    with open('players.txt', 'r') as f:
-        for line in f:
-            player_list.append(line.strip())
-
-    t = Tournament(player_list)
     while True:
         cmd = input('>')
         words = cmd.split(' ')
